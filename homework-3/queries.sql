87cd9ecfe257ff3189df875f5dd6b0bcd2cd606d
miwnaya <mikgerasimov@gmail.com>
2023-07-26 22:49:14 +0300

    As requested

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,16 +1,31 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-
+SELECT customers.company_name, CONCAT(employees.first_name, ' ', employees.last_name) as full_name
+FROM orders
+INNER JOIN customers USING (customer_id)
+INNER JOIN employees USING (employee_id)
+INNER JOIN shippers ON orders.ship_via = shippers.shipper_id
+WHERE customers.city = 'London' AND employees.city = 'London' AND shippers.company_name = 'United Packag'
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
--- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
+--имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
--- Отсортировать результат по возрастанию количества оставшегося товара.
-
+-- Отсортировать результат по возрастанию количества оставшегося товара
+SELECT product_name, units_in_stock,suppliers.contact_name, suppliers.phone
+FROM products
+INNER JOIN suppliers USING (supplier_id)
+INNER JOIN categories USING(category_id)
+WHERE discontinued <> 1 AND units_in_stock < 25 AND categories.category_name in ('Dairy Products', 'Condiments')
+ORDER BY units_in_stock
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
+SELECT company_name FROM customers
+WHERE NOT EXISTS(SELECT customer_id FROM orders WHERE customers.customer_id=orders.customer_id)
 
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+SELECT DISTINCT products.product_name FROM order_details
+INNER JOIN products USING (product_id)
+WHERE EXISTS (SELECT 1 FROM orders WHERE order_details .quantity = 10 AND orders.order_id=order_details.order_id)
